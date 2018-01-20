d2aa580d70f25e95b984ad700550232211c470cf
Artemy Magarin <magar.art@yandex.ru>
2018-01-20 22:10:26 +0300

    initial

---
+++ js/carusel.js
@@ -0,0 +1,33 @@
+$(function(){
+
+	$('#step1').css('left', 0);
+	$('#step2').css('left', 1 * $('body').width() + 'px');
+	$('#step3').css('left', 2 * $('body').width() + 'px');
+	$('#step4').css('left', 3 * $('body').width() + 'px');
+
+	var stepNum = 0;
+
+	$.slideSwitch = function(){
+		
+		stepNum = (stepNum+1)%4;
+		
+		$('#step1').animate({
+			'left': (0-stepNum * $('body').width())+'px'
+		}, $('body').width())
+		$('#step2').animate({
+			'left': ((1-stepNum) * $('body').width())+'px'
+		}, $('body').width())
+		$('#step3').animate({
+			'left': ((2-stepNum) * $('body').width())+'px'
+		}, $('body').width())
+		$('#step4').animate({
+			'left': ((3-stepNum) * $('body').width())+'px'
+		}, $('body').width())
+
+	}
+
+	$('.nextButton').on('click', $.slideSwitch)
+
+	$(window).on('resize', function() {stepNum--; $.slideSwitch();})
+
+});
